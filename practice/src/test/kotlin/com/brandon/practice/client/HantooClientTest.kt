4edd535bfeff15b9ef106d696958fe84752bac1f
brandon.kwon <brandon.kwon@kakaocorp.com>
2023-02-22 00:01:37 +0900

    .yml파일로 secret변수들 주입

---
+++ practice/src/test/kotlin/com/brandon/practice/client/HantooClientTest.kt
@@ -0,0 +1,49 @@
+package com.brandon.practice.client
+
+import com.brandon.practice.hantoo.HantooClient
+import com.brandon.practice.hantoo.HantooPriceTemplate.*
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+
+@TestPropertySource(locations = ["classpath:application.yml"])
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+)
+class HantooClientTest {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Autowired
+    lateinit var hantooClient: HantooClient
+
+    @Value("\${auth.appkey}")
+    lateinit var appkey: String
+
+    @Value("\${auth.appsecret}")
+    lateinit var appsecret: String
+
+    @Value("\${auth.accessToken}")
+    lateinit var accessToken: String
+
+
+    @Test
+    fun getDomesticPriceTest() {
+        logger.info("appkey: ${appkey}")
+        logger.info("appsecret: ${appsecret}")
+
+        val domesticPriceRequest = DomesticPriceRequest(
+            request=DomesticPriceRequest.Request(
+                fid_input_iscd = "331520"
+            ),
+            header = DomesticPriceRequest.Header(
+                authorization = "Bearer ${ACCESS_TOKEN_SAMPLE}"
+            )
+        )
+
+        hantooClient.getPrice(domesticPriceRequest)
+
+    }
+}
